a873c7a46ac4e27b4f99429aeeebc05c4f06d654
Smurfhvr8ght <andres.winson@binus.ac.id>
2025-03-13 10:41:19 +0700

    Add files via upload

---
+++ OOP_FinalProject_Sem2/src/Hand.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Hand {
+    private ArrayList<String> currentHand = new ArrayList<>();
+    private int numOfCard;
+    public Hand() {
+        this.numOfCard = 0;
+    }
+    public Hand(String[] list) {
+        this.currentHand.addAll(Arrays.asList(list));
+        this.numOfCard += 1;
+    }
+    public String toString() {
+        return ""+this.currentHand;
+    }
+
+    //add card to hand
+    public void addCard(String card){
+        this.currentHand.add(card);
+        this.numOfCard += 1;
+    }
+    public void addCard(String[] card){
+        this.currentHand.addAll(Arrays.asList(card));
+        this.numOfCard += 1;
+    }
+    public void removeCard(String card){
+        if (this.currentHand.contains(card)) {
+            this.currentHand.remove(card);
+            this.numOfCard -= 1;
+        }
+    }
+
+    //remove card from hand
+    public void removeCard(String[] card){
+        boolean have = true;
+        for (String i : card) {
+            if (!this.currentHand.contains(i)) {
+                have = false;
+                break;
+            }
+        }
+        if (have) {
+            for (String i : card) {
+                this.currentHand.remove(i);
+                this.numOfCard -= 1;
+            }
+        }
+    }
+}
